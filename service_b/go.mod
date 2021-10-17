c16c9fa96451ca25fc64bcb5030a725dc1bdfd36
smith-30 <kohei.misu@techtouch.co.jp>
2021-10-17 18:25:46 +0900

    mod:

--- service_b/go.mod
+++ service_b/go.mod
@@ -2,4 +2,7 @@ module github.com/smith-30/monorepo/service_b
 
 go 1.16
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/google/uuid v1.3.0
+)
--- service_b/main.go
+++ service_b/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/smith-30/monorepo/service_b/model"
 )
 
 func main() {
 	u, _ := model.NewUser()
 	fmt.Printf("%#v\n", u)
+
+	spew.Dump(u)
 }
