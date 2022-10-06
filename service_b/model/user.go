fe2cf70f2bc004141616fb2641e159df1c1cd294
smith-30 <kohei.misu@techtouch.co.jp>
2022-10-06 23:41:54 +0900

    mod: test

--- service_b/model/user.go
+++ service_b/model/user.go
@@ -1,15 +1,11 @@
 package model
 
-import (
-	"github.com/google/uuid"
-)
-
 type User struct {
 	ID string
 }
 
 func NewUser() (*User, error) {
 	return &User{
-		ID: uuid.New().String(),
+		ID: "",
 	}, nil
 }
